aa955d46bb0f50c1ec69f5a490ac673b66f7a98e
andreymuskat <andrey.strel.2001@gmail.com>
2023-05-24 11:09:35 +0300

    add migrations

---
+++ AVSoftTest.DAL/AVSoftTest.DAL/Migrations/20230524075741_MigrationDB.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AVSoftTest.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Counters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<int>(type: "int", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Counters", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Counters",
+                columns: new[] { "Id", "Key", "Value" },
+                values: new object[,]
+                {
+                    { 1, 1, 1 },
+                    { 2, 1, 2 },
+                    { 3, 1, 3 },
+                    { 4, 2, 1 },
+                    { 5, 2, 1 },
+                    { 6, 2, 3 },
+                    { 7, 2, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Counters");
+        }
+    }
+}
