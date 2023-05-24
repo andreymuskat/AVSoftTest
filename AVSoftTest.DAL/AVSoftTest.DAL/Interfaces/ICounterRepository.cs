5bf2827d10d16e21a12350e53e42c13b26e3cf2f
andreymuskat <andrey.strel.2001@gmail.com>
2023-05-24 11:08:10 +0300

    interfaces DAL

---
+++ AVSoftTest.DAL/AVSoftTest.DAL/Interfaces/ICounterRepository.cs
@@ -0,0 +1,11 @@
+﻿using AVSoftTest.DAL.Models;
+
+namespace AVSoftTest.DAL.Interfaces
+{
+    public interface ICounterRepository
+    {
+        public List<CounterEntity> GetAllCounters();
+
+        public void AddCounter(CounterEntity counters);
+    }
+}
